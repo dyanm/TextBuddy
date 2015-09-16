5cd7d44e7950dd743ea3fa8f960908ce5aea0193
dyanm <daltonyan@gmail.com>
2015-09-16 23:53:17 +0800

    Implemented test case for clearFileContents method
    Compares the return value and checks if they are equal.

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -8,28 +8,31 @@ import org.junit.runners.MethodSorters;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TextBuddyTest {
 
+	private String fileName = "testFile.txt";
+	
 	@Test
 	public void a_testLoadFile() {
-		TextBuddy.clearFileContents("testFile.txt");
-		String[] fileName = {"testFile.txt"};
-		TextBuddy.loadFile(fileName);
-		assertTrue(new File("testFile.txt").exists());
+		TextBuddy.clearFileContents(fileName);
+		String[] args = {"testFile.txt"};
+		TextBuddy.loadFile(args);
+		assertTrue(new File(fileName).exists());
 	}
 	
 	@Test
 	public void b_testAddNewLine() {	
-		assertEquals(TextBuddy.addNewLine("little brown fox"), "added to testFile.txt: \"little brown fox\"\n");
-		assertEquals(TextBuddy.addNewLine("jumped over the moon"), "added to testFile.txt: \"jumped over the moon\"\n");
+		assertEquals(TextBuddy.addNewLine("little brown fox"), "added to " + fileName + ": \"little brown fox\"\n");
+		assertEquals(TextBuddy.addNewLine("jumped over the moon"), "added to " + fileName + ": \"jumped over the moon\"\n");
 	}
 	
 	@Test
 	public void c_testDeleteExistingLine() {
-		assertEquals(TextBuddy.deleteExistingLine("2"), "deleted from testFile.txt: \"jumped over the moon\"\n");
+		assertEquals(TextBuddy.deleteExistingLine("2"), "deleted from " + fileName + ": \"jumped over the moon\"\n");
 	}
 
 	@Test
 	public void d_testClearFileContents() {
-		fail("Not yet implemented"); // TODO
+		TextBuddy.clearFileContents(fileName);
+		assertEquals(TextBuddy.displayFileContents(), fileName + " is empty.\n");
 	}
 	
 	@Test
